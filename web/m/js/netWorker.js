2e68eeecfa3d5d1edb07a955b1e0cfcbba2f8cef
codansYC <yuanchao@chushi007.com>
2017-12-27 21:27:35 +0800

    a

--- web/m/js/netWorker.js
+++ web/m/js/netWorker.js
@@ -4,6 +4,7 @@
 //处理网络请求
 var basicUrl = "../../"
 function request(url,params,respBlock) {
+    alert(url)
     $.post(basicUrl + url, params, function (response, status) {
         if (status == 'success') {
             var resp = $.parseJSON(response);
