ae83ac2ffb93e3513aa674df412ca68ac9bb758c
codansYC <yuanchao@chushi007.com>
2018-01-12 11:41:17 +0800

    q

--- web/m/js/netWorker.js
+++ web/m/js/netWorker.js
@@ -1,15 +1,3 @@
 /**
  * Created by lekuai on 17/4/7.
  */
-//处理网络请求
-var basicUrl = "../../"
-function request(url,params,respBlock) {
-    $.post(basicUrl + url, params, function (response, status) {
-        if (status == 'success') {
-            var resp = $.parseJSON(response);
-            respBlock(resp)
-        } else {
-            showModel('操作失败')
-        }
-    });
-}
